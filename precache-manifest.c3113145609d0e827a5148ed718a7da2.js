3f19b1c19b50784a39a4d89503b6891d1cbdaff0
Radoslaw Matlak <radek1999@onet.com.pl>
2019-07-31 16:07:15 +0200

    Updates

---
+++ precache-manifest.c3113145609d0e827a5148ed718a7da2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be13376c0943e61cd5dbd831a7c90ed7",
+    "url": "/ToDoApp/index.html"
+  },
+  {
+    "revision": "b6d37894da40854a7e34",
+    "url": "/ToDoApp/static/css/main.5b8caa34.chunk.css"
+  },
+  {
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "/ToDoApp/static/js/2.a5adb38c.chunk.js"
+  },
+  {
+    "revision": "b6d37894da40854a7e34",
+    "url": "/ToDoApp/static/js/main.5146b26d.chunk.js"
+  },
+  {
+    "revision": "fb1067834985d33aa2a1",
+    "url": "/ToDoApp/static/js/runtime~main.f2c45213.js"
+  }
+]);
